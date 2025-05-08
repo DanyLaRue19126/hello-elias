1535c41898a0ac24536e7bd60ca7731b0b88ad37
Danylarue19126 <elias.fouadaziz@capellinisauro.edu.it>
2025-05-08 07:33:42 +0000

    Aggiunta stampa da VECTOR

--- helloelias.java
+++ helloelias.java
@@ -1,10 +1,16 @@
 public class helloelias{
     public static void main(String[] args) {
         System.out.println("Hello Elias");
-        char[] lettere = {'D' 'A' 'N' 'I' 'E' 'L' 'E'};
+        Vector<Charger> lettere = new Vector<>();
+        lettere.add('D');
+        lettere.add('A');
+        lettere.add('N');
+        lettere.add('I');
+        lettere.add('E');
+        lettere.add('L');
+        lettere.add('E');
         for (char lettera : lettere) {
-            System.out.println(lettera + " ");
+            System.out.println(lettera);
         }
-      
-}
+       }
 }
