4fa4a75d8802c821d354e8e66887fa0bf4b3fe7c
Danylarue19126 <elias.fouadaziz@capellinisauro.edu.it>
2025-05-08 07:26:32 +0000

    Aggiunta stampa da ARRAY STATICO

--- helloelias.java
+++ helloelias.java
@@ -1,7 +1,10 @@
 public class helloelias{
     public static void main(String[] args) {
         System.out.println("Hello Elias");
-        System.out.println("8 marzo 2007");
-        System.out.println("Inter");
-    }
+        char[] lettere = {'D' 'A' 'N' 'I' 'E' 'L' 'E'};
+        for (char lettera : lettere) {
+            System.out.println(lettera + " ");
+        }
+      
+}
 }
