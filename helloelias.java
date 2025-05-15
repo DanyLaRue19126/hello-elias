05be7530affb276c006273433da9d93147e72705
Abruzzes <federico.abruzzese@capellinisauro.edu.it>
2025-05-15 06:44:49 +0000

    Modifica del compagno

--- helloelias.java
+++ helloelias.java
@@ -2,6 +2,6 @@ public class helloelias{
     public static void main(String[] args) {
         System.out.println("Hello Elias");
         System.out.println("8 marzo 2007");
-        System.out.println("Inter");
+        System.out.println("Abruzzese capo");
     }
 }
